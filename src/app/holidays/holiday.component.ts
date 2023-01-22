f523d1fb09120ef114e62f7ca937b0dbe660edcf
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-01-22 16:02:34 +0100

    add legacy module

--- src/app/holidays/holiday.component.ts
+++ src/app/holidays/holiday.component.ts
@@ -1,17 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { HolidaysService } from './holidays.service';
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-holiday',
   standalone: true,
-  template: `<h2>Holidays</h2>
+  template: `<h2 transloco="holidays.title"></h2>
     <ul *ngIf="holidays$ | async as holidays">
       <li *ngFor="let holiday of holidays">{{ holiday.title }}</li>
     </ul>`,
-  imports: [NgIf, AsyncPipe, NgFor],
+  imports: [NgIf, AsyncPipe, NgFor, TranslocoModule, JsonPipe],
 })
 export class HolidayComponent {
   #holidaysService = inject(HolidaysService);
   protected holidays$ = this.#holidaysService.find();
+
+  translation = inject(TranslocoService).getTranslation();
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import {
+  Translation,
+  TRANSLOCO_CONFIG,
+  TRANSLOCO_LOADER,
+  translocoConfig,
+  TranslocoModule,
+} from '@ngneat/transloco';
+import { importProvidersFrom, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -12,5 +21,23 @@ bootstrapApplication(AppComponent, {
         loadChildren: () => import('./app/holidays/holiday-routes'),
       },
     ]),
+    {
+      provide: TRANSLOCO_CONFIG,
+      useValue: translocoConfig({
+        availableLangs: ['en'],
+      }),
+    },
+    {
+      provide: TRANSLOCO_LOADER,
+      useFactory: () => {
+        const httpClient = inject(HttpClient);
+        return {
+          getTranslation(lang: string): Observable<Translation> {
+            return httpClient.get<Translation>(`/assets/${lang}.json`);
+          },
+        };
+      },
+    },
+    importProvidersFrom([TranslocoModule]),
   ],
 });
